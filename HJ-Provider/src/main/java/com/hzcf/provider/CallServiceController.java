7b88d98dc20ee9e7ea8fbd3546c06fb82a9ccf80
huhl <huhl@gitlab.hzbd.com>
2018-05-10 14:14:21 +0800

    添加决策引擎配置&修改接口列表

--- HJ-Provider/src/main/java/com/hzcf/provider/CallServiceController.java
+++ HJ-Provider/src/main/java/com/hzcf/provider/CallServiceController.java
@@ -6,17 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
+import com.hzcf.service.MS_RuleEngineService;
 import com.hzcf.service.MS_VariableService;
 @RestController
-public class CallVarController {
+public class CallServiceController {
 	@Autowired
 	MS_VariableService variableService;
+	@Autowired
+	MS_RuleEngineService ruleService;
 	@Value("${spring.cloud.config.discovery.serviceId}")
 	String serviceId;
 	@Value("${eureka.client.serviceUrl.defaultZone}")
 	String defaultZone;
 	
-	@RequestMapping("/")
+	@RequestMapping("/var")
     public JSONObject var(){
 		System.out.println(serviceId);
 		System.out.println(defaultZone);
@@ -25,4 +28,14 @@ public class CallVarController {
 		System.out.println(ret.toJSONString());
         return ret;
     }
+	
+	@RequestMapping("/rule")
+	public JSONObject rule(){
+		System.out.println(serviceId);
+		System.out.println(defaultZone);
+		JSONObject request = JSONObject.parseObject("{\"param\":{\"X_NEXTDATA_R02\":0,\"Z_LAWXP_R01\":0,\"X_NEXTDATA_R01\":\"\",\"X_91_R01\":0,\"X_NEXTDATA_R04\":0,\"Z_LAWXP_R03\":0,\"X_NEXTDATA_R03\":false,\"Z_LAWXP_R02\":0,\"X_NEXTDATA_R05\":\"\",\"Z_LAWXP_R04\":0,\"X_91_R02\":0,\"X_91_R03\":0,\"X_SUANHUA_R06\":0,\"X_SUANHUA_R05\":0,\"X_SUANHUA_R02\":0,\"Z_GEOTMT_R02\":false,\"Z_EMAY_R04\":\"0\",\"Z_EMAY_R01\":\"0\",\"X_SUANHUA_R08\":0,\"Z_EMAY_R03\":\"0\",\"X_SUANHUA_R07\":0,\"X_RONG360_R03\":0,\"X_RONG360_R02\":0,\"Z_FPJK_R03\":\"1\",\"X_GEOTMT_R05\":\"\",\"X_GEOTMT_R04\":\"\"},\"ruleId\":\"176fd7f763df49d2a42179eb6ab24932\",\"groupKey\":\"R_NEW_3\",\"taskId\":\"BOR20170804000626\"}");
+		JSONObject ret = ruleService.rule(request);
+		System.out.println(ret.toJSONString());
+		return ret;
+	}
 }
