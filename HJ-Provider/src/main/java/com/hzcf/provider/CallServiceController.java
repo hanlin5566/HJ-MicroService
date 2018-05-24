61714daff499d9c18c3c36bc0bb10f6fbfae3c93
huhl <huhl@gitlab.hzbd.com>
2018-05-24 18:35:30 +0800

    添加验证返回状态码接口

--- HJ-Provider/src/main/java/com/hzcf/provider/CallServiceController.java
+++ HJ-Provider/src/main/java/com/hzcf/provider/CallServiceController.java
@@ -2,10 +2,14 @@ package com.hzcf.provider;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
+import com.hzcf.operation.base.result.ResponseCode;
+import com.hzcf.operation.base.result.ResponseData;
 import com.hzcf.service.MS_RuleEngineService;
 import com.hzcf.service.MS_VariableService;
 @RestController
@@ -18,6 +22,16 @@ public class CallServiceController {
 	String serviceId;
 	@Value("${eureka.client.serviceUrl.defaultZone}")
 	String defaultZone;
+	
+	@GetMapping("/response/{id}")
+	public ResponseData auth(@PathVariable Integer id){
+		if(id > 0){
+			return ResponseData.fail(ResponseCode.codeOf(id));
+		}else{
+			JSONObject request = JSONObject.parseObject("{\"taskId\":\"123\",\"ruleId\":\"321\",\"companyCode\":\"9c4f92a0ce684ccfa960ec3bb6dcc7f9\",\"service\":\"zcPublicService.blacklist\",\"param\":{\"riskItems\":[{\"riskItemValue\":\"430224198902203316\",\"riskTime\":\"2017\",\"sourceCode\":\"10\",\"riskTypeCode\":\"10\",\"riskType\":\"资料虚假类\",\"riskItemType\":\"身份证号\",\"riskItemTypeCode\":\"101010\",\"source\":\"宜信个人客户\"}]}}");
+			return ResponseData.ok(request);
+		}
+	}
 	
 	@RequestMapping("/var")
     public JSONObject var(){
