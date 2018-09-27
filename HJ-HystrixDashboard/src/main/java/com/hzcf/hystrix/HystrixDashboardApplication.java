c5023f4830163032120067f285491d4261891330
huhl <huhanlin@huizhongcf.com>
2018-09-27 17:14:51 +0800

    hystrix流量面板初始化

---
+++ HJ-HystrixDashboard/src/main/java/com/hzcf/hystrix/HystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.hzcf.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringCloudApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class,args);
+	}
+}
