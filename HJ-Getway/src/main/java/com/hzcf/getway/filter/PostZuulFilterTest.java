ff0185feda9f4570200547131d2a56cb303b8e08
huhl <huhanlin@huizhongcf.com>
2018-09-27 17:11:08 +0800

    网关fitler初始化

---
+++ HJ-Getway/src/main/java/com/hzcf/getway/filter/PostZuulFilterTest.java
@@ -0,0 +1,74 @@
+package com.hzcf.getway.filter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+/**
+ * Create by hanlin on 2018年5月30日
+ * 仅用于测试
+ **/
+@Component
+public class PostZuulFilterTest extends ZuulFilter {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	/**
+	 * shouldFilter：返回一个boolean类型来判断该过滤器是否要执行，所以通过此函数可实现过滤器的开关。
+	 * 在上例中，我们直接返回true，所以该过滤器总是生效。
+	 */
+	@Override
+	public boolean shouldFilter() {
+		return false;
+	}
+
+	/*
+	 * run：过滤器的具体逻辑。需要注意，这里我们通过ctx.setSendZuulResponse(false)令zuul过滤该请求，
+	 * 不对其进行路由，然后通过ctx.setResponseStatusCode(401)设置了其返回的错误码，
+	 * 当然我们也可以进一步优化我们的返回，比如，通过ctx.setResponseBody(body)对返回body内容进行编辑等。
+	 */
+	@Override
+	public Object run() {
+		// double a = 100/0;
+		RequestContext ctx = RequestContext.getCurrentContext();
+		InputStream stream = ctx.getResponseDataStream();
+		try {
+			String body = StreamUtils.copyToString(stream, Charset.forName("UTF-8"));
+			//请求的返回值
+			logger.error(body);
+			//返回值取出后，如不再次设值，则返回内容
+//			ctx.setResponseBody(ResponseData.fail(ResponseCode.RESOURCE_NOT_FOUND).toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		logger.error("后置过滤器-->网关收到应答后触发。");
+		return null;
+	}
+
+	/**
+	 * filterType： 返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：
+	 * pre：可以在请求被路由之前调用 routing：在路由请求时候被调用 post：在routing和error过滤器之后被调用
+	 * error：处理请求时发生错误时被调用
+	 */
+	@Override
+	public String filterType() {
+		return FilterConstants.POST_TYPE;
+	}
+
+	/**
+	 * filterOrder：通过int值来定义过滤器的执行顺序
+	 */
+	@Override
+	public int filterOrder() {
+		return 8;
+	}
+
+}
